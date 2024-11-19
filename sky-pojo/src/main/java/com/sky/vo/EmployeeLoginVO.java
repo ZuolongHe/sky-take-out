441f3eaff2208374cdfae123ec2e8f7883c55a3a
ZuolongHe <hzldragon68@163.com>
2024-11-19 15:27:37 +0800

    苍穹外卖11-19提交测试

--- sky-pojo/src/main/java/com/sky/vo/EmployeeLoginVO.java
+++ sky-pojo/src/main/java/com/sky/vo/EmployeeLoginVO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
 @Data
-@Builder
+@Builder   // 用于简化对象创建过程，提供“流式API”的构造方式
 @NoArgsConstructor
 @AllArgsConstructor
 @ApiModel(description = "员工登录返回的数据格式")
