bfc3646d3bf47faf3ab280a608bf8920458b23ba
ZuolongHe <hzldragon68@163.com>
2024-11-23 21:50:45 +0800

    苍穹外卖菜品分页查询提交测试

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,18 @@
+package com.sky.controller.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title sky-take-out
+ * @Author hzl
+ * @Date 2024/11/23 21:44
+ * @Description 菜品相关接口
+ */
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+public class DishController {
+
+}
