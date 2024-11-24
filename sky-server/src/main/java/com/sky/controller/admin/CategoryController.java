d61952dbd1c692f2756e43cd113cd3e5fb5f6fb5
ZuolongHe <hezuolong161460@163.com>
2024-11-24 19:26:01 +0800

    苍穹外卖分类分页查询提交测试

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,19 @@
+package com.sky.controller.admin;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title sky-take-out
+ * @Author hzl
+ * @Date 2024/11/24 19:18
+ * @Description
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "分类相关接口")
+public class CategoryController {
+}
